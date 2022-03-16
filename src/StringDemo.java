e2229cb82982e025fad5e24279067dcf70f49dd6
mr_str <80831520+motorin-romchik@users.noreply.github.com>
2022-03-16 23:53:26 +0700

    Initial commit

---
+++ src/StringDemo.java
@@ -0,0 +1,12 @@
+public class StringDemo {
+    public static void main (String args[]){
+
+        String str1 = new String("В Java строки - объекты.");
+        String str2 = "Их можно создавать различными способами.";
+        String str3 = (str2);
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
